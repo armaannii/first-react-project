bfc5691373bab5548d1e8a2d137e3511d5375f38
armaannii <shahinrayhan92@gmail.com>
2026-09-14 12:10:30 +0600

    fix stack card desing

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,32 +1,25 @@
 import { RxCross2 } from "react-icons/rx";
 import type { Technology } from "../types/TechnologiesType";
 
-
-
 interface StackProps {
   stack: Technology[];
   onRemove: (id: string) => void;
   onRemoveAll: () => void;
 }
 
-
-const Stack = ( {stack,  onRemove, onRemoveAll}: StackProps) => {
+const Stack = ({ stack, onRemove, onRemoveAll }: StackProps) => {
   return (
     <div className="rounded-lg border border-dotted border-slate-300 p-6">
-
       {/* Header */}
       <div className="">
         <div>
-          <h2 className="font-bold text-base">
-            Your Stack
-          </h2>
+          <h2 className="font-bold text-base">Your Stack</h2>
 
           <p className="text-xs text-[#94A3B8]">
-            {stack.length}{" "}
-            {stack.length === 1 ? "technology" : "technologies"} selected
+            {stack.length} {stack.length === 1 ? "technology" : "technologies"}{" "}
+            selected
           </p>
         </div>
-
       </div>
 
       {/* Empty Stack */}
@@ -35,16 +28,13 @@ const Stack = ( {stack,  onRemove, onRemoveAll}: StackProps) => {
           <p>Your stack is empty</p>
         </div>
       ) : (
-
         /* Stack Items */
         <div className="mt-4 space-y-3">
-
           {stack.map((technology) => (
             <div
               key={technology.id}
               className="flex items-center justify-between rounded-lg border border-slate-200 p-3"
             >
-
               <div className="flex items-center gap-2">
                 <img
                   src={technology.icon}
@@ -52,9 +42,16 @@ const Stack = ( {stack,  onRemove, onRemoveAll}: StackProps) => {
                   className="w-6 h-6 object-contain"
                 />
 
-                <span className="text-xs font-semibold">
-                  {technology.name}
-                </span>
+                <div className="flex flex-col gap-3">
+                  
+                    <span className="text-xs font-bold leading-0">
+                      {technology.name}
+                    </span>
+                    <span className="text-[7px] text-[#94A3B8] font-bold leading-0">
+                      {technology.category}
+                    </span>
+                  
+                </div>
               </div>
 
               <button
@@ -63,23 +60,20 @@ const Stack = ( {stack,  onRemove, onRemoveAll}: StackProps) => {
               >
                 <RxCross2 />
               </button>
-              
-
             </div>
           ))}
           <div className="w-full">
-                {stack.length > 0 && (
-                  <button
-                    onClick={onRemoveAll}
-                    className="w-full btn btn-wide text-sm text-red-500 font-semibold border-red-300 rounded-xl mt-8">
-                    Remove All
-                  </button>
-                )}
+            {stack.length > 0 && (
+              <button
+                onClick={onRemoveAll}
+                className="w-full btn btn-wide text-sm text-red-500 font-semibold border-red-300 rounded-xl mt-8"
+              >
+                Remove All
+              </button>
+            )}
           </div>
-
         </div>
       )}
-
     </div>
   );
 };
